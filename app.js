c49272f4831d215f29410ee4d853b9b33bd81cad
Satyam Lal <satyamlal@users.noreply.github.com>
2025-01-24 23:09:12 +0530

    Started working on index.ejs to render front-end

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("../Cozio/models/listing");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/cozio";
 
@@ -17,23 +18,31 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.send("Everything is working fine!");
 });
 
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My new Villa",
-    description: "Buy this beach",
-    price: 790,
-    location: "Playa Venao, Azuero Peninsula",
-    country: "Panama",
-  });
-
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("Testing Successfull");
+app.get("/listings", async (req, res) => {
+  const allListing = await Listing.find({});
+  res.render("listings/index.ejs", { allListing });
 });
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My new Villa",
+//     description: "Buy this beach",
+//     price: 790,
+//     location: "Playa Venao, Azuero Peninsula",
+//     country: "Panama",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("Testing Successfull");
+// });
 
 app.listen(3000, () => {
   console.log("Server is listening to port 3000.");
