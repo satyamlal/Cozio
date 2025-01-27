20ef9fc68f30410389c2d1f973430ed9f97d8e64
Satyam Lal <satyamlal@users.noreply.github.com>
2025-01-27 10:47:49 +0530

    Started working on EJS Mate for develope boiletplates

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("../Cozio/models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/cozio";
 
@@ -23,6 +24,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
   res.send("Everything is working fine!");
