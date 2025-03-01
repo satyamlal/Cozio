9f8f3e475341c9cfc2aacdd1a7859483e75caa9b
Satyam Lal <satyamlal@users.noreply.github.com>
2025-03-01 13:58:43 +0530

    All the reviews will be deleted now, if any listing is deleted from the database

--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 // Import Mongoose to define and work with MongoDB schemas
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 // Define the schema for a listing
 const listingSchema = new Schema({
@@ -11,8 +12,8 @@ const listingSchema = new Schema({
   description: String,
   image: {
     type: String,
-    default: "/images/default-image.jpg",
-    set: (v) => (v.trim() === "" ? "/images/default-image.jpg" : v),
+    default: "/images/cozio-default-image.jpg",
+    set: (v) => (v.trim() === "" ? "/images/cozio-default-image.jpg" : v),
   },
   price: Number,
   location: String,
@@ -23,6 +24,18 @@ const listingSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+// This is a mongoose middleware, which will automatically run database-level logic after a specific query operation occurs.
+// This middle is going to delete all the reviews for the Deleted listing.
+listingSchema.post("findOneAndDelete", async (listing) => {
+  // Above code:
+  // This is a Mongoose post-hook that listens for the "findOneAndDelete" operation on documents that follow the listingSchema.
+  // When It Runs: It executes after a listing document has been found and deleted using a method like Listing.findOneAndDelete().
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+    // { _id: { $in: listing.review } : This is a MongoDB query using the $in operator. It tells MongoDB to find all reviews whose _id is included in the array listing.review.
+  }
 });
 
 // Export the model to use it in other parts of the app
